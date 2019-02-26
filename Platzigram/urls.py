b7a89c068a1e7593933d637aafa0f2fe94ffd69c
Jorge Luis <cokimaniac@gmail.com>
2019-02-26 15:47:16 -0400

    Extendiendo el model de usuario documentacion de apoyo https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#extending-the-existing-user-model

--- Platzigram/urls.py
+++ Platzigram/urls.py
@@ -1,6 +1,7 @@
 
 """Platzigram URL module"""
-
+#Django
+from django.contrib import admin
 from django.urls import path
 
 from Platzigram import views as local_views
@@ -8,6 +9,8 @@ from posts import views as posts_views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     path('helloworld/', local_views.helloworld ),
     path('sortnum/', local_views.sortnum),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),

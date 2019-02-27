823a104e26ec362673f308e0fe7d7db412b9d7f1
Jorge Luis <cokimaniac@gmail.com>
2019-02-27 00:19:30 -0400

    Creacion del modelo de posts documentacion de apoyo https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-MEDIA_ROOT

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,21 @@
+"""Posts admin classes."""
+
+#Django
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'photo', 'created', 'modified')
+    search_fields = ('title', 'user__username')
+    list_filter = ('created', 'modified', 'user__is_active')
+
+    fieldsets = (
+        ('Post', {
+            'fields': (
+                ('photo', 'title'),
+            )
+        }),
+    )

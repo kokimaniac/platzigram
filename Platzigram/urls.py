87425c11fa55d72dd3d47f2b3153dd028830ffec
Jorge Luis <cokimaniac@gmail.com>
2019-02-27 12:02:16 -0400

    Login documentacion de apoyo https://docs.djangoproject.com/en/2.1/topics/auth/default/

--- Platzigram/urls.py
+++ Platzigram/urls.py
@@ -8,14 +8,18 @@ from django.urls import path
 
 from Platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('helloworld/', local_views.helloworld ),
-    path('sortnum/', local_views.sortnum),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('helloworld/', local_views.helloworld, name='helloworld'),
+    path('sortnum/', local_views.sortnum, name='sort'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
 
-    path('posts/', posts_views.list_posts),
+    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('users/login/', users_views.login_view, name='login'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

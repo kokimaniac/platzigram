e96193cd0bb0d9032d793ffddbd1dc06ef9cbef7
Jorge Luis <cokimaniac@gmail.com>
2019-02-26 23:16:34 -0400

    Explorando el dashboard de administracion

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+"""User admin classes."""
 
-# Register your models here.
+#Django
+from django.contrib import admin
+from users.models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile Admin."""
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
+
+    list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')

b7d11a193101246b8d16dede186f6cba930f40c0
Jorge Luis <cokimaniac@gmail.com>
2019-02-26 23:40:47 -0400

    Dashboard de administracion documentacion de apoyo https://docs.djangoproject.com/en/2.1/topics/auth/customizing/ https://docs.djangoproject.com/en/2.1/ref/contrib/admin/

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,10 @@
 
 #Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+#Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -13,3 +17,40 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
 
     list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),
+                )
+        }),
+        ('Extra Info', {
+            'fields': (
+                ('website'),
+                ('phone_number'),
+                ('biography')
+            )
+        }),
+        ('Meta Data', {
+            'fields': (
+                ('created', 'modified'),
+            )
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+class ProfileInLine(admin.StackedInline):
+    """Profile inline admin for users."""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Adds profile admin to base user admin"""
+    inlines = (ProfileInLine,)
+
+    list_display = ('username', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)

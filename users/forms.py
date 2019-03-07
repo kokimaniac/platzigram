b96e4d7d37771693c9f8f95876765d93a635c0e0
Jorge Luis <cokimaniac@gmail.com>
2019-03-07 15:03:19 -0400

    Validación de formularios documentación de apoyo https://docs.djangoproject.com/en/2.1/ref/forms/widgets/ https://docs.djangoproject.com/en/2.1/ref/forms/validation/ https://docs.djangoproject.com/en/2.1/topics/forms/

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,111 @@
 #Django
 from django import forms
 
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+class SignupForm(forms.Form):
+    """Sign up form."""
+    username = forms.CharField(
+        min_length=4,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Username',
+                'class': 'form-control',
+                'required': True
+                }
+        )
+    )
+    password = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password',
+                'class': 'form-control',
+                'required': True
+            }
+        )
+    )
+    password_confirmation = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget = forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password Confirmation',
+                'class': 'form-control',
+                'required': True
+            }
+        )
+    )
+
+    first_name = forms.CharField(
+        min_length=3,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'First name',
+                'class': 'form-control',
+                'required': True
+                }
+            )
+        )
+    last_name = forms.CharField(
+        min_length=3,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Last name',
+                'class': 'form-control',
+                'required': True
+                }
+        )
+    )
+
+    email = forms.CharField(
+        min_length=6,
+        max_length=70,
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "email",
+                "class": "form-control",
+                'required': True
+            }
+        )
+    )
+
+    def clean_username(self):
+        """Username must be unique."""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use!')
+        return username
+
+    def clean(self):
+        """Verify password confirmation match."""
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise forms.ValidationError('Password confirmation does not match!')
+
+        return data
+
+    def save(self):
+        """Create user and profile."""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user =User.objects.create_user(**data)
+        profile = Profile(user=user)
+        profile.save()
+
+
 class ProfileForm(forms.Form):
     """Profile form."""
     website = forms.URLField(max_length=200, required=True)
--- users/views.py
+++ users/views.py
@@ -4,13 +4,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-#Exceptions
-from django.db.utils import IntegrityError
-#Model
-from django.contrib.auth.models import User
-from users.models import Profile
 #Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 @login_required
 def update_profile(request):
@@ -58,30 +53,17 @@ def login_view(request):
 def signup(request):
     """Signup view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_conf = request.POST['passwd_conf']
-
-        if passwd!=passwd_conf:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match!'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd_conf)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already taken!'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=SignupForm()
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 @login_required
 def logout_view(request):

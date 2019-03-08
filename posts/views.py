6d101872298b3569c2dc5b24877a6aaf9448f869
Jorge Luis <cokimaniac@gmail.com>
2019-03-08 19:04:36 -0400

    Platzigram subido a github

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,6 @@
 
 #Django
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@
 from django.contrib.auth import authenticate, login, logout, views as auth_views
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, FormView, UpdateView
 #Models
